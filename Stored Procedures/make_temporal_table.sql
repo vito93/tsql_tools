51e15c72edb782985c6c88823cbf2541dec5cce5
Victor Klein <vikklein93@gmail.com>
2024-09-04 01:23:59 +0400

    make_temporal_table edit

--- Stored Procedures/make_temporal_table.sql
+++ Stored Procedures/make_temporal_table.sql
@@ -1,24 +1,33 @@
 ﻿CREATE PROCEDURE tsql_tools.make_temporal_table
-	@table_name sysname
-	, @schema_name sysname
-	, @start_time_column sysname = 'sys_start_time'
-	, @end_time_column sysname = 'sys_end_time'
-	, @period_cols_type sysname = 'DATETIME2'
+	@table_name SYSNAME
+	, @schema_name SYSNAME
+	, @start_time_column SYSNAME = 'sys_start_time'
+	, @end_time_column SYSNAME = 'sys_end_time'
+	, @period_cols_type SYSNAME = 'DATETIME2'
 	, @period_cols_length TINYINT = 3
 AS
 BEGIN
 	SET NOCOUNT ON;
 
 	DECLARE @schema_id INT, @table_id INT, @full_table_name VARCHAR(256) = CONCAT(@schema_name, '.', @table_name)
-	, @temporal_type INT, @SQL VARCHAR(MAX), @period_cols_full_type sysname;
+	, @temporal_type INT, @SQL VARCHAR(MAX), @period_cols_full_type SYSNAME, @history_table_name SYSNAME;
 
-	DECLARE @start_period_default_constraint sysname = CONCAT('DF_', @schema_name, '_', @table_name, '_', @start_time_column),
-	@end_period_default_constraint sysname = CONCAT('DF_', @schema_name, '_', @table_name, '_', @end_time_column);
+	DECLARE @start_period_default_constraint SYSNAME = CONCAT('DF_', @schema_name, '_', @table_name, '_', @start_time_column),
+	@end_period_default_constraint SYSNAME = CONCAT('DF_', @schema_name, '_', @table_name, '_', @end_time_column);
 
+	-- Lookup table and schema IDs
 	SELECT @schema_id = sch.schema_id, @table_id = tab.object_id
 	FROM sys.schemas AS sch
 	LEFT JOIN sys.tables AS tab ON sch.schema_id = tab.schema_id
 	WHERE sch.name = @schema_name AND tab.name = @table_name;
+
+	/* Throw errors if:
+
+	1. Input schema does not exist;
+	2. Input table does not exist;
+	3. Input table is already temporal.
+
+	*/
 
 	IF @schema_id IS NULL
 	BEGIN;
@@ -28,6 +37,13 @@ BEGIN
 	IF (OBJECT_ID(@table_id) IS NULL)
 	BEGIN;
 		THROW 50002, 'Input table does not exist', 1;
+	END;
+
+	SELECT @temporal_type = temporal_type FROM sys.tables WHERE name = @table_name AND schema_id = @schema_id;
+
+	if @temporal_type = 2
+	BEGIN;
+		THROW 50003, 'Input table is already temporal', 1;
 	END;
 
 	SET @period_cols_full_type = CONCAT(@period_cols_type, '(', CAST(@period_cols_length AS CHAR(1)), ')'); 
@@ -53,27 +69,35 @@ BEGIN
 	ADD %s %s NOT NULL CONSTRAINT %s DEFAULT %s;
 	END;', @full_table_name, @end_time_column, @period_cols_full_type, @end_period_default_constraint, '''9999-12-31''');
 	
-	EXEC (@SQL);
 	END;
-	
-	
-	PRINT @temporal_type;
-	
-	IF @temporal_type <> 2
-	BEGIN
+
+	EXEC (@SQL);
+
+	SET @SQL = FORMATMESSAGE('ALTER TABLE %s
+	ADD PERIOD FOR SYSTEM_TIME(%s, %s);
+	', @full_table_name, @start_time_column, @end_time_column);
+
 	EXEC ('
 	ALTER TABLE dbo.environment
 	ADD PERIOD FOR SYSTEM_TIME(sys_start_time, sys_end_time);
 	');
-	
-	ALTER TABLE dbo.environment
-	ALTER COLUMN sys_start_time
+
+	EXEC (@SQL);
+
+	SET @SQL = FORMATMESSAGE('
+	ALTER TABLE %s
+	ALTER COLUMN %s
 	ADD HIDDEN;
 	
-	ALTER TABLE dbo.environment
-	ALTER COLUMN sys_end_time
-	ADD HIDDEN
+	ALTER TABLE %s
+	ALTER COLUMN %s
+	ADD HIDDEN;
+	', @full_table_name, @start_time_column, @full_table_name, @end_time_column);
 	
-	ALTER TABLE dbo.environment SET (SYSTEM_VERSIONING = ON (HISTORY_TABLE=dbo.environment_history, DATA_CONSISTENCY_CHECK=ON));
+	EXEC(@SQL);
+
+	SET @SQL = FORMATMESSAGE('ALTER TABLE %s SET (SYSTEM_VERSIONING = ON (HISTORY_TABLE=%s_history, DATA_CONSISTENCY_CHECK=ON));', @full_table_name, @full_table_name);
+
+	EXEC(@SQL);
 END;
 GO
